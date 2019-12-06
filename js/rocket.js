eac933a43556cd0d7340a36df4d41e6f5ea683be
unknown <jonathansmadsen@gmail.com>
2019-12-06 14:35:35 +0100

    5.

--- js/rocket.js
+++ js/rocket.js
@@ -4,11 +4,6 @@ var $win = $(window);
 $win.on('scroll', function() {
     var top = $win.scrollTop();
     var $wave = Math.sin(0.0034*top);
-    $rocket.css('transform', 'translate(' + -$wave*700 + 'px, 500px)');
-})
-
-$win.on('scroll', function() {
-    var top = $win.scrollTop();
-    var rotate = Math.sin(0.0034*top);
-    $rocket.css('transform', 'rotate(' + -$wave*700 + 'deg)');
+    var rotate = Math.sin(0.0034*top+40);
+    $rocket.css('transform', 'translate(' + -$wave*700 + 'px, 500px) rotate(' + 40*rotate + 'deg)');
 })

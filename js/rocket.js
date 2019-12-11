8835eab1bd584535db7b0c1d63680988d31a293c
jona737a <jonathansmadsen@gmail.com>
2019-12-11 12:13:06 +0100

    13.

--- js/rocket.js
+++ js/rocket.js
@@ -2,16 +2,26 @@ var $rocket = $('#rocket');
 var $win = $(window);
 var width = $(window).width();
 
+
 $win.on('scroll', function() {
     var top = $win.scrollTop();
-    var $wave = Math.sin(0.0034*top);
+    var wave = Math.sin(0.0034*top);
     var rotate = Math.sin(0.0034*top+40);
-   
-    if (top == 0) {
-        $rocket.css('transform', 'translate(' + -$wave*(width*0.25) + 'px, 550px) rotate(' + 0 + 'deg)');
+    if(width < 600){
+        if (top == 0) {
+            $rocket.css('transform', 'translate(' + -wave*(width*0.25) + 'px, 300px ) rotate(' + 0 + 'deg)');
+        }else{
+            $rocket.css('transform', 'translate(' + -wave*(width*0.25) + 'px, 300px) rotate(' + 40*rotate + 'deg)');
+        }
     }else{
-        $rocket.css('transform', 'translate(' + -$wave*(width*0.25) + 'px, 550px) rotate(' + 40*rotate + 'deg)');
+        if (top == 0) {
+            $rocket.css('transform', 'translate(' + -wave*(width*0.25) + 'px, 550px ) rotate(' + 0 + 'deg)');
+        }else{
+            $rocket.css('transform', 'translate(' + -wave*(width*0.25) + 'px, 550px) rotate(' + 40*rotate + 'deg)');
+        }
     }
+    
+    
     
 })
 

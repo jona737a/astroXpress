0f383359bbb163abfa787c57a5f25f41446b8993
jona737a <jonathansmadsen@gmail.com>
2019-12-17 14:25:42 +0100

    22.

--- js/rocket.js
+++ js/rocket.js
@@ -3,10 +3,14 @@ var $win = $(window);
 var width = $(window).width();
 
 
+
 $win.on('scroll', function() {
     var top = $win.scrollTop();
     var wave = Math.sin(0.0034*top);
     var rotate = Math.sin(0.0034*top+40);
+
+    // "Media query" that splits at 600px
+
     if(width < 600){
         if (top <= 0) {
             $rocket.css('transform', 'translate(' + -wave*(width*0.25) + 'px, 380px ) rotate(' + 0 + 'deg)');
@@ -23,32 +27,15 @@ $win.on('scroll', function() {
 });
 
 
-$win.on('scroll', function() { 
-    if ($(window).scrollTop() >= $( 
-      '.testy').offset().top + $('.testy'). 
-        outerHeight() - window.innerHeight) { 
-        
-            // Her skal der aendres image
-         //   document.getElementById('myImage').src='pic_bulbon.gif'
-            document.getElementById('rocket').src = 'Assets/svg/Rocket_FrontpageReverse.svg';
-     
-    }
-}); 
-// 
-$win.on('scroll', function() { 
-    console.log("Tes", this.scrollTop)
-    if ($(window).scrollTop() == 0) {   
-        document.getElementById('rocket').src = 'Assets/svg/Rocket_Frontpage.svg'; 
-    }
-});
+
 
 
 var didScroll;
-var lastScrollTop = 8;
-var navbarHeight = $('header').outerHeight();
+var lastScrollTop = 0;
+var navbarHeight = $('.header').outerHeight();
 var $info = $('.info');
 var $about = $('.about');
-var $header = $('header');
+var $header = $('.header');
 
 $win.scroll(function(event){
     didScroll = true;
@@ -83,3 +70,18 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
+
+$win.on('scroll', function() { 
+    if ($(window).scrollTop() >= $( 
+      '.footer').offset().top + $('.footer'). 
+        outerHeight() - window.innerHeight) { 
+            document.getElementById('rocket').src = 'Assets/svg/Rocket_FrontpageReverse.svg';
+     
+    }
+}); 
+
+$win.on('scroll', function() { 
+    if ($(window).scrollTop() == 0) {   
+        document.getElementById('rocket').src = 'Assets/svg/Rocket_Frontpage.svg'; 
+    }
+});
